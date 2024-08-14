8a4380c21a5ed2edb1aab164954b06d38410984c
devclaudiocassimiro <claudio.silva@jala.university>
2024-08-14 17:56:07 -0300

    refactor: change two files to one

--- src/utils/validations/loginSchema.ts
+++
@@ -1,6 +0,0 @@
-import * as yup from 'yup';
-
-export const loginSchema = yup.object().shape({
-  email: yup.string().email('Email inválido').required('O email é obrigatório'),
-  password: yup.string().required('A senha é obrigatória'),
-});
