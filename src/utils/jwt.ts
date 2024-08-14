7982adb141a313c516d7c8b7081bf4872ac28d9e
devclaudiocassimiro <claudio.silva@jala.university>
2024-08-14 17:40:50 -0300

    fix: fix some problems

--- src/utils/jwt.ts
+++ src/utils/jwt.ts
@@ -4,7 +4,13 @@ import jwt from 'jsonwebtoken';
 
 const secret = process.env.JWT_SECRET || 'your_jwt_secret';
 
-export const generateToken = (payload: any): string => {
+interface TokenPayload {
+  id: string;
+  email: string;
+  tipo: string;
+}
+
+export const generateToken = (payload: TokenPayload): string => {
   return jwt.sign(payload, secret, { expiresIn: '1h' });
 };
 
