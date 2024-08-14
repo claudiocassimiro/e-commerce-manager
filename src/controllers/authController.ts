72a089971fa52104fb41614704db1498d40fe511
devclaudiocassimiro <claudio.silva@jala.university>
2024-08-14 17:56:23 -0300

    refactor: remove yup validation from controller

--- src/controllers/authController.ts
+++ src/controllers/authController.ts
@@ -2,8 +2,6 @@ import { Request, Response, NextFunction } from 'express';
 import * as yup from 'yup';
 import { AuthService } from '../services/authService';
 import { AppError } from '../utils/appError';
-import { registerSchema } from '../utils/validations/registerSchema';
-import { loginSchema } from '../utils/validations/loginSchema';
 import { PrismaClientKnownRequestError } from '@prisma/client/runtime/library';
 
 export class AuthController {
@@ -19,8 +17,6 @@ export class AuthController {
     next: NextFunction,
   ): Promise<void> {
     try {
-      await registerSchema.validate(req.body, { abortEarly: false });
-
       const { email, password, name, tipo } = req.body;
 
       const user = await this.authService.register(email, password, name, tipo);
@@ -47,8 +43,6 @@ export class AuthController {
 
   async login(req: Request, res: Response, next: NextFunction): Promise<void> {
     try {
-      await loginSchema.validate(req.body, { abortEarly: false });
-
       const { email, password } = req.body;
       const token = await this.authService.login(email, password);
       if (token) {
