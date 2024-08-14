abbdd91f1c331a07a754426aee839dd9bad7695f
devclaudiocassimiro <claudio.silva@jala.university>
2024-08-14 16:06:29 -0300

    feat: implements autorizeMiddleware

---
+++ src/middlewares/autorizeMiddleware.ts
@@ -0,0 +1,22 @@
+import { Request, Response, NextFunction } from 'express';
+import { AppError } from '../utils/appError';
+
+type UserType = 'ADMIN' | 'CLIENTE';
+
+export const authorize = (allowedRoles: UserType[]) => {
+  return (req: Request, res: Response, next: NextFunction) => {
+    const user = req.user;
+
+    if (!user) {
+      return next(new AppError('Usuário não autenticado', 401));
+    }
+
+    if (!allowedRoles.includes(user.tipo)) {
+      return next(
+        new AppError('Você não tem permissão para acessar esta rota', 403),
+      );
+    }
+
+    next();
+  };
+};
