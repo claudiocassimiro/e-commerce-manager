547fe3c2afe5ebfd2d75003fd1134d414c8d4121
devclaudiocassimiro <claudio.silva@jala.university>
2024-08-14 13:39:45 -0300

    refactor: add yup validation

--- src/controllers/authController.ts
+++ src/controllers/authController.ts
@@ -1,9 +1,9 @@
-/* eslint-disable @typescript-eslint/no-unused-vars */
-// src/controllers/authController.ts
-
 import { Request, Response, NextFunction } from 'express';
+import * as yup from 'yup';
 import { AuthService } from '../services/authService';
 import { AppError } from '../utils/appError';
+import { registerSchema } from '../utils/validations/registerSchema';
+import { loginSchema } from '../utils/validations/loginSchema';
 
 export class AuthController {
   private authService: AuthService;
@@ -17,18 +17,28 @@ export class AuthController {
     res: Response,
     next: NextFunction,
   ): Promise<void> {
-    const { email, password, name, tipo } = req.body;
     try {
+      await registerSchema.validate(req.body, { abortEarly: false });
+
+      const { email, password, name, tipo } = req.body;
       const user = await this.authService.register(email, password, name, tipo);
       res.status(201).json({ user });
     } catch (error) {
+      if (error instanceof yup.ValidationError) {
+        const validationErrors = error.errors.join(', ');
+        return next(
+          new AppError(`Erro de validação: ${validationErrors}`, 400),
+        );
+      }
       next(new AppError('Erro ao registrar usuário', 500));
     }
   }
 
   async login(req: Request, res: Response, next: NextFunction): Promise<void> {
-    const { email, password } = req.body;
     try {
+      await loginSchema.validate(req.body, { abortEarly: false });
+
+      const { email, password } = req.body;
       const token = await this.authService.login(email, password);
       if (token) {
         res.status(200).json({ token });
@@ -36,6 +46,12 @@ export class AuthController {
         next(new AppError('Credenciais inválidas', 401));
       }
     } catch (error) {
+      if (error instanceof yup.ValidationError) {
+        const validationErrors = error.errors.join(', ');
+        return next(
+          new AppError(`Erro de validação: ${validationErrors}`, 400),
+        );
+      }
       next(new AppError('Erro ao fazer login', 500));
     }
   }
