e5b7a9361e668dd988ce461508a4dca6d13a1014
devclaudiocassimiro <claudio.silva@jala.university>
2024-08-15 10:11:27 -0300

    refactor: fix problem with validation

--- src/utils/validations/authValidation.ts
+++ src/utils/validations/authValidation.ts
@@ -1,19 +1,29 @@
 import * as yup from 'yup';
 
 export const loginSchema = yup.object().shape({
-  email: yup.string().email('Email inválido').required('O email é obrigatório'),
-  password: yup.string().required('A senha é obrigatória'),
+  body: yup.object({
+    email: yup
+      .string()
+      .email('Email inválido')
+      .required('O email é obrigatório'),
+    password: yup.string().required('A senha é obrigatória'),
+  }),
 });
 
 export const registerSchema = yup.object().shape({
-  email: yup.string().email('Email inválido').required('O email é obrigatório'),
-  password: yup
-    .string()
-    .min(6, 'A senha deve ter pelo menos 6 caracteres')
-    .required('A senha é obrigatória'),
-  name: yup.string().required('O nome é obrigatório'),
-  tipo: yup
-    .mixed()
-    .oneOf(['ADMIN', 'CLIENTE'], 'Tipo de usuário inválido')
-    .required('O tipo de usuário é obrigatório'),
+  body: yup.object({
+    email: yup
+      .string()
+      .email('Email inválido')
+      .required('O email é obrigatório'),
+    password: yup
+      .string()
+      .min(6, 'A senha deve ter pelo menos 6 caracteres')
+      .required('A senha é obrigatória'),
+    name: yup.string().required('O nome é obrigatório'),
+    tipo: yup
+      .mixed()
+      .oneOf(['ADMIN', 'CLIENTE'], 'Tipo de usuário inválido')
+      .required('O tipo de usuário é obrigatório'),
+  }),
 });
